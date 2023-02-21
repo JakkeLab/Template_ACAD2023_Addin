0df6c724e86646f325f7b5c95ca73869b3a8e06f
JakkeLab <itsjakk3@gmail.com>
2023-02-21 23:48:38 +0900

    First commit

---
+++ Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using acadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace MyAddin
+{
+    public class Command
+    {
+        public class Commands
+        {
+
+            [CommandMethod("TEST")]
+            public void Test()
+            {
+                var doc = acadApp.DocumentManager.MdiActiveDocument;
+                var db = doc.Database;
+                var ed = doc.Editor;
+                using (var tr = db.TransactionManager.StartTransaction())
+                {
+                    tr.Commit();
+                }
+            }
+        }
+    }
+}
